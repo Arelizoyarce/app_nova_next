291daea8605e2cbb669c29d7935cab99f2cb1070
Areliz Oyarce <arelizuwc@gmail.com>
2024-09-26 10:58:01 -0500

    feat: Add stack

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      
+      <iframe>
+        
+      </iframe>
     </div>
   );
 }
